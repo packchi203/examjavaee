c6c0ac1e27a13178f21d2da757fce27ac32ec2de
packchi203 <96831035+packchi203@users.noreply.github.com>
2023-08-07 20:47:23 +0700

    exam

---
+++ examjava/src/main/java/com/example/examjava/ExamJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.examjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamJavaApplication.class, args);
+	}
+
+}
